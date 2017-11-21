1a46777c8567579fe267de26434f806b26bfe000
nm <nicolas.monaldi@cube.com.ar>
2017-11-21 13:11:17 -0300

    Se agrega IntalacionInmuebleDAO

---
+++ Modulo/src/main/java/ar/com/iua/modulo/model/persistence/dao/IInstalacionInmuebleDAO.java
@@ -0,0 +1,10 @@
+package ar.com.iua.modulo.model.persistence.dao;
+
+import ar.com.iua.modulo.model.InstalacionInmueble;
+import ar.com.iua.modulo.persistence.dao.IGenericDAO;
+
+/**
+ * Created by mnicolas on 20/11/17.
+ */
+public interface IInstalacionInmuebleDAO extends IGenericDAO <InstalacionInmueble,Integer> {
+}
