1b358f25988a499579596fcece82eac14f587cbc
nm <nicolas.monaldi@cube.com.ar>
2018-02-25 21:03:01 -0300

    AND BLUE DA BA DEE DA BA DAA

---
+++ Modulo/src/main/java/ar/com/iua/modulo/business/plan/operaciones/OperadorIngresos.java
@@ -0,0 +1,44 @@
+package ar.com.iua.modulo.business.plan.operaciones;
+
+import ar.com.iua.modulo.business.model.PlanResultado;
+import ar.com.iua.modulo.business.services.Interfaces.IPlanService;
+import ar.com.iua.modulo.model.Plan_Criterio;
+
+/**
+ * Created by mnicolas on 25/02/18.
+ */
+public class OperadorIngresos extends OperadorAbstracto {
+
+    private String operacionLogica = "";
+    private boolean match;
+
+    @Override
+    void cargarOperador(PlanResultado resultados, Plan_Criterio criterio, IPlanService planService) {
+        operacionLogica = criterio.getConstanteOperador().getConstante();
+        match = false;
+        switch (operacionLogica) {
+            case "MAYOR":
+                this.match = resultados.obtenerFamilia().getIngresoNetoFamiliar() > Integer.parseInt(criterio.getValor());
+                break;
+            case "MENOR":
+                this.match = resultados.obtenerFamilia().getIngresoNetoFamiliar() < Integer.parseInt(criterio.getValor());
+                break;
+            case "IGUAL":
+                this.match = resultados.obtenerFamilia().getIngresoNetoFamiliar() == Integer.parseInt(criterio.getValor());
+                break;
+        }
+    }
+
+    @Override
+    PlanResultado operar(PlanResultado resultado, Plan_Criterio criterio) {
+        if (this.match) {
+            resultado.addPuntaje(criterio.getPuntaje());
+        }
+        return resultado;
+    }
+
+    @Override
+    boolean verificarRequerido(PlanResultado resultado, Plan_Criterio criterio) {
+        return this.match;
+    }
+}
